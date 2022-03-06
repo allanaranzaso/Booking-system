0f5cab525d5b3e9806eea87403a2940d5bc715f5
Allan Aranzaso <67307011+allanaranzaso@users.noreply.github.com>
2022-03-06 09:39:51 -0800

    Revert "add firebase dependencies"
    This reverts commit cdd5d8d2976b8818e1de7337123f37ba358eb337.

--- server.ts
+++ server.ts
@@ -1,46 +1,41 @@
-// Import dependencies
-import { Db } from "mongodb";
-import {Express, Request, Response} from "express";
-import * as bodyParser from "body-parser";
-import * as path from 'path';
-import { MongoClient } from 'mongodb';
-import * as dotenv from 'dotenv';
-// import authMiddleware from './middleware/auth-middleware'; // uncomment when ready to implement api auth
+// import express from 'express';
+// import bodyParser from 'body-parser';
+// import path from 'path';
 
-const express = require("express");
-dotenv.config(); // load .env file for local dev
+const { Db } = require("mongodb");
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-// Create new express server
-const app: Express = express();
 
-const { API_URL, PORT,
-        CUSTOMERS_DB, CUSTOMER_COLLECTION} = process.env; // Get the variables from the .env file
-// const client = new MongoClient(API_URL as string); // Create new instance of MongoClient
-const client = new MongoClient(API_URL as string);
+const app = express();
 
+const { API_URL, PORT } = process.env;
+const client = new MongoClient(API_URL);
+console.log(__dirname)
 app.use(express.static(path.join(__dirname, 'client/build'))); // tells express where the files to be served are located
 app.use(bodyParser.json()); // parses json object along with post request. Adds body prop to request param... access the json properties by using req.body.jsonProp
-// app.use("/", authMiddleware); // uncomment this when ready to implement login! for now we'll expose the endpoints
 
 /**
- * Get a list of all customer information
+ * Get a projects information
  * @param name - the name of the project to retrieve
  */
-app.get('/api/v1/customers', async (req: Request , res: Response) => {
+app.get('/api/portfolio/:name', async (req: typeof express.Request, res: typeof express.Response) => {
 
-    await client.connect(); // connect to mongo
-    const db = client.db(CUSTOMERS_DB as string);
-    const customers = await db.collection(CUSTOMER_COLLECTION as string).findOne( {_firstName: "Allan"} );
-    res.status(200).json(customers);
-    console.log(`GET successful`);
-    await client.close(); // close client
+    await withDB(async (db: typeof Db) => {
+        const projectName = req.params.name;
+        const project = await db.collection('Comments').findOne({projectName: projectName});
 
+        res.status(200).json(project);
+    }, res);
 
 });
 
-app.post('/api/portfolio/:name/upvote', async (req: Request, res: Response) => {
+app.post('/api/portfolio/:name/upvote', async (req: typeof express.Request, res: typeof express.Response) => {
 
-    await withDB( async (db: Db) => {
+    await withDB( async (db: typeof Db) => {
         const projectName = req.params.name;
         // find the project
         const project: any = await db.collection('Comments').findOne({ projectName: projectName});
@@ -60,11 +55,11 @@ app.post('/api/portfolio/:name/upvote', async (req: Request, res: Response) => {
 
 });
 
-app.post('/api/portfolio/:name/add-comment', async(req: Request, res: Response) => {
+app.post('/api/portfolio/:name/add-comment', async(req: typeof express.Request, res: typeof express.Response) => {
     const projectName = req.params.name; // url param
     const { username, text } = req.body; // request body destructured into the appropriate const
 
-    await withDB( async (db: Db) => {
+    await withDB( async (db: typeof Db) => {
         // connect to the collection and query the db to find the correct project
         const projectInfo: any = await db.collection('Comments').findOne({ projectName: projectName });
 
@@ -87,29 +82,23 @@ app.post('/api/portfolio/:name/add-comment', async(req: Request, res: Response) 
 /**
  * Serves the index.html file in the build folder
  */
-app.get('*', (req: Request, res: Response) => {
+app.get('*', (req: typeof express.Request, res: typeof express.Response) => {
     res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
 app.listen(PORT, () => console.log(`App is listening on port ${PORT}`));
 
 // Connects to mongodb and uses callback to determine what the function will perform
-// CURRENTLY BROKEN :(
-const withDB = async (operations: (database: Db) => Promise<void>, res: Response) => {
-
-    try {
-        console.dir("connecting to the mongo server...");
+const withDB = async (operations: any, res: typeof express.Response) => {
+    try{
         await client.connect(); // connect to mongo
-        console.log("connected successfully... connecting to the db...");
-        const db = client.db(CUSTOMERS_DB as string);
-        console.log("connected to db....");
-        await operations(db);
-        console.log('got out of the operations function call...')
-        await client.close(); // close client
 
+        const db = client.db("Projects");
+
+        await operations(db);
+
+        await client.close(); // close client
     } catch (err) {
-        res.status(500).json({message: 'Error connecting to db!!', err});
+        res.status(500).json({ message: 'Error connecting to db', err });
     }
 }
-
-
